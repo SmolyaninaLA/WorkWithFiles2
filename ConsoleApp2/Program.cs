2ba4838ae85ddb6894b27f921167aa54242f1fd2
Smolyaninala <Smolyaninala@arkona36.ru>
2021-04-14 07:15:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+ 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // получим папку
+
+        Console.WriteLine("Введите путь ");
+        string pathDir = Console.ReadLine();
+
+
+        if (!Directory.Exists(pathDir))
+        {
+            Console.WriteLine("Нет указанго пути");
+        }
+        else
+        {
+            string[] subdir = Directory.GetDirectories(pathDir);
+            DirectoryInfo dr;
+
+            foreach (string sd in subdir)
+            {
+                dr = new DirectoryInfo(sd);
+                try
+                {
+                    long size = GetDirSize(dr);
+                    Console.WriteLine("Размер директории {0}", size);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка при подсчете размера директории {0}", ex.Message);
+
+                }
+            }
+        }
+    }
+
+    
+    
+    public static long GetDirSize(DirectoryInfo dir)
+    {
+        long size = 0;
+        FileInfo[] fileInDir = dir.GetFiles();
+       
+        // посчитали размер файлов
+        foreach(FileInfo f in fileInDir)
+        {
+            size += f.Length;
+        }
+
+        // размер в директории
+        DirectoryInfo[] Dir = dir.GetDirectories();
+
+        foreach (DirectoryInfo d in Dir)
+        {
+            size += GetDirSize(d);
+        }
+
+        return size;
+    }
+   
+}
+
+
+
---
+++ ConsoleApp2/TaskDirectory2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+
+
+    public static class TaskDirectory
+    {
+        public static long GetDirSize(DirectoryInfo dir)
+        {
+            long size = 0;
+            FileInfo[] fileInDir = dir.GetFiles();
+            DirectoryInfo[] Dir = dir.GetDirectories();
+
+            // посчитали размер файлов
+            foreach(FileInfo f in fileInDir)
+            {
+                size = size + f.Length;
+            }
+
+            // размер в директории
+            foreach (DirectoryInfo d in Dir)
+            {
+                size = size + GetDirSize(d);
+            }
+
+            return size;
+        }
+    }
+
+
